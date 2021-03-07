d7bccecc829542e1bd773946ed8575348b1eb955
Marcos Vinicius <marcosvinicius@MacBook-Pro-de-Marcos.local>
2021-03-07 16:24:38 -0300

    (feat, broken): add microservice types

--- pkg/apis/seratos/types.go
+++ pkg/apis/seratos/types.go
@@ -1,6 +1,8 @@
 package seratos
 
-import metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
 
 // +genclient
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
@@ -19,4 +21,63 @@ type FooList struct {
 	metav1.ListMeta
 
 	Items []Foo
+}
+
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// Microservice Resource
+type Microservice struct {
+	metav1.TypeMeta
+	metav1.ObjectMeta
+
+	Spec MicroserviceSpec
+}
+
+// MicroserviceSpec define the specification of the microservice object
+type MicroserviceSpec struct {
+	Container string
+	Env       []Env
+}
+
+// Env defines enviroment varialbles for the microservices
+type Env struct {
+	name  string
+	value string
+}
+
+// MicroserviceTopology defines the topology for a service
+type MicroserviceTopology struct {
+	Proxys   []Proxy
+	Secutiry MicroserviceSecutiry
+}
+
+// Proxy defines some proxys for the service
+type Proxy struct {
+	service string
+	host    string
+	ports   ProxyPorts
+}
+
+// ProxyPorts defines the proxy ports
+type ProxyPorts struct {
+	TCP   int32
+	HTTP2 int32
+}
+
+// MicroserviceSecutiry defines microservice secutiry parameters
+type MicroserviceSecutiry struct {
+	Policy string
+	Blocks []string
+	Allow  []string
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// MicroserviceList is a list of Microservices objects.
+type MicroserviceList struct {
+	metav1.TypeMeta
+	metav1.ListMeta
+
+	Items []Microservice
 }
--- pkg/apis/seratos/v1beta1/types.go
+++ pkg/apis/seratos/v1beta1/types.go
@@ -21,3 +21,63 @@ type FooList struct {
 
 	Items []Foo `json:"items" protobuf:"bytes,2,rep,name=items"`
 }
+
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+// +k8s:openapi-gen=true
+
+// Microservice Resource
+type Microservice struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty" protobuf:"bytes,1,opt,name=metadata"`
+
+	Spec MicroserviceSpec `json:"spec,omitempty" protobuf:"bytes,2,opt,name=spec"`
+}
+
+// MicroserviceSpec define the specification of the microservice object
+type MicroserviceSpec struct {
+	Container string `json:"container" protobuf:"bytes,1,opt,name=container"`
+	Env       []Env  `json:"env" protobuf:"bytes,2,opt,name=env"`
+}
+
+// Env defines enviroment varialbles for the microservices
+type Env struct {
+	Name  string `json:"name" protobuf:"bytes,1,opt,name=name"`
+	Value string `json:"value" protobuf:"bytes,2,opt,name=value"`
+}
+
+// MicroserviceTopology defines the topology for a service
+type MicroserviceTopology struct {
+	Proxys   []Proxy              `json:"proxys" protobuf:"bytes,1,opt,name=proxys"`
+	Secutiry MicroserviceSecutiry `json:"security" protobuf:"bytes,2,opt,name=security"`
+}
+
+// Proxy defines some proxys for the service
+type Proxy struct {
+	Service string     `json:"service" protobuf:"bytes,1,opt,name=service"`
+	Host    string     `json:"host" protobuf:"bytes,2,opt,name=host"`
+	Ports   ProxyPorts `json:"ports" protobuf:"bytes,3,opt,name=ports"`
+}
+
+// ProxyPorts defines the proxy ports
+type ProxyPorts struct {
+	TCP   int32 `json:"tcp" protobuf:"bytes,1,opt,name=tcp"`
+	HTTP2 int32 `json:"http2" protobuf:"bytes,1,opt,name=http2"`
+}
+
+// MicroserviceSecutiry defines microservice secutiry parameters
+type MicroserviceSecutiry struct {
+	Policy string   `json:"policity" protobuf:"bytes,1,opt,name=policity"`
+	Blocks []string `json:"blocks" protobuf:"bytes,1,opt,name=blocks"`
+	Allow  []string `json:"allow" protobuf:"bytes,1,opt,name=allow"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// MicroserviceList is a list of Microservices objects.
+type MicroserviceList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty" protobuf:"bytes,1,opt,name=metadata"`
+
+	Items []Microservice `json:"items" protobuf:"bytes,2,rep,name=items"`
+}
